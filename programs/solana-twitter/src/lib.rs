4fe935ccee2c8d750518b0c95a438fcbc9cef17a
Hubris <dan@LAPTOP-LIL83ON6>
2024-05-08 18:52:58 +0200

    more code

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::system_program;
 
 declare_id!("2FoavwpHFnnfV3hHppLDQ6pFSvhbGrxd4QCmy9iWAwn7");
 
@@ -6,14 +7,38 @@ declare_id!("2FoavwpHFnnfV3hHppLDQ6pFSvhbGrxd4QCmy9iWAwn7");
 pub mod solana_twitter {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn send_tweet(ctx: Context<SendTweet>, topic: String, content: String) -> ProgramResult {
+        let tweet: &mut Account<Tweet> = &mut ctx.accounts.tweet;
+        let author: &Signer = &ctx.accounts.author;
+        let clock: Clock = Clock::get().unwrap();
+
+        if topic.chars().count() > 50 {
+            return Err(ErrorCode::TopicTooLong.into())
+        }
+
+        if content.chars().count() > 280 {
+            return Err(ErrorCode::ContentTooLong.into())
+        }
+
+        tweet.author = *author.key;
+        tweet.timestamp = clock.unix_timestamp;
+        tweet.topic = topic;
+        tweet.content = content;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct SendTweet<'info> {
+    #[account(init, payer = author, space = Tweet::LEN)] // this constraint defines who pays for rent and how much space the tweet account will occupy
+    pub tweet: Account<'info, Tweet>,
+    #[account(mut)] // this constraint declares that the account that signs the transaction to create the tweet can be mutated to pay for rent
+    pub author: Signer<'info>,
+    #[account(address = system_program::ID)] // this constraint is here to ensure that the correct system program is passed in the transaction that creates the tweet
+    pub system_program: AccountInfo<'info>,
+   // pub system_program: Program<'info, System>, // same as line above
+}
 
 // 1. Define the structure of the Tweet account.
 #[account]
@@ -39,4 +64,12 @@ impl Tweet {
         + TIMESTAMP_LENGTH // Timestamp.
         + STRING_LENGTH_PREFIX + MAX_TOPIC_LENGTH // Topic.
         + STRING_LENGTH_PREFIX + MAX_CONTENT_LENGTH; // Content.
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The provided topic should be 50 characters long maximum.")]
+    TopicTooLong,
+    #[msg("The provided content should be 280 characters long maximum.")]
+    ContentTooLong,
 }
