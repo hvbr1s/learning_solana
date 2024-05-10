b5b39fa8de8e97a42734186a32120d4e4583aa20
Hubris <dan@LAPTOP-LIL83ON6>
2024-05-10 10:15:24 +0200

    files

---
+++ app/src/main.js
@@ -0,0 +1,15 @@
+// CSS.
+import './main.css'
+
+// Routing.
+import { createRouter, createWebHashHistory } from 'vue-router'
+import routes from '/home/dan/solana/dapp/solana-twitter/app/src/route.js'
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+})
+
+// Create the app.
+import { createApp } from 'vue'
+import App from './App.vue'
+createApp(App).use(router).mount('#app')
